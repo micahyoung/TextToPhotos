e4588d8082da8cb1a21a1bc6de049289ef7cc98f
Micah Young <micah.young@gmail.com>
2025-09-24 06:21:20 -0400

    Initial Commit

---
+++ Text2Photos/Text2PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text2PhotosApp.swift
+//  Text2Photos
+//
+//  Created by user on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Text2PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
